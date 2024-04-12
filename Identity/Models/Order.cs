f16d5114d76d1d6e28e421f5206aa0f247024aad
Haneul-Song <haneulisstudying@gmail.com>
2024-04-11 22:32:01 -0600

    update

--- Identity/Models/Order.cs
+++ Identity/Models/Order.cs
@@ -9,12 +9,12 @@ namespace Identity.Models
         public int transaction_ID { get; set; }
 
         [ForeignKey("customer_ID")]
-        public string? customer_ID { get; set; }
+        public int customer_ID { get; set; }
         public DateTime? date { get; set; }
         public string? day_of_week { get; set; }
         public int? time { get; set; }
         public string? entry_mode { get; set; }
-        public decimal amount { get; set; }
+        public int? amount { get; set; }
         public string? type_of_transaction { get; set; }
         public string? country_of_transaction { get; set; }
         public string? shipping_address { get; set; }
